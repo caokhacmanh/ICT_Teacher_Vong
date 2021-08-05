719d09bf42d7dcc1f7ef8bf68e12d3de463f890f
caokhacmanh <caokhacmanh2018@gmail.com>
2021-08-05 08:45:17 +0700

    week18 done

---
+++ week18/dogcat.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[20002];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("dogcat.inp", "r", stdin);
+    freopen("dogcat.out", "w", stdout);
+    int d, n, tot = 0, tmp = 0;
+    bool check = true;
+    cin >> d >> n;
+    for(int j = 0; j < d; ++j){
+        for(int i = 0; i < n; ++i){
+            cin >> arr[i];
+        }
+        while(check){
+            for(int i = 0; i < n - 1; ++i){
+                
+            }
+        }
+    
+    }
+}
