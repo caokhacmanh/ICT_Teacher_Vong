f4091b8b64d92140a455ead690c3a02de24d98c0
caokhacmanh <caokhacmanh2018@gmail.com>
2021-08-12 07:51:35 +0700

    doin week20

---
+++ week20/tk1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, sum = 0;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("tk1.inp", "r", stdin);
+    freopen("tk1.out", "w", stdout);
+    cin >> n;
+    
+}
